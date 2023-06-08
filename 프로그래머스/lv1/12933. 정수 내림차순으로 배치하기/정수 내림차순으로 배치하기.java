3b4fb932f0a8fb933d5540a8dff461f839659535
yelimkin <77622069+yelimkin@users.noreply.github.com>
2023-06-08 23:38:30 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 0.36 ms, Memory: 77.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+  public long solution(long n) {
+    long answer = 0;
+    ArrayList<Long> num = new ArrayList<>();
+
+    while (n > 10) {
+      long add_num = n % 10;
+      num.add(add_num);
+      n /= 10;
+    }
+
+    num.add(n);
+    Collections.sort(num, Collections.reverseOrder());
+
+    int i = 0;
+    while (i < num.size()) {
+      answer += (Math.pow(10, i) * num.get((num.size() - 1) - i));
+      i++;
+    }
+
+    return answer;
+  }
+}
