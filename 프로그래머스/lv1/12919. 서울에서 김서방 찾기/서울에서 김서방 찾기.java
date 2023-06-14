778fd8ffd6e3fd03ca0eabd65c3fdd8e822b14af
yelimkin <77622069+yelimkin@users.noreply.github.com>
2023-06-14 21:56:10 +0900

    Update 서울에서 김서방 찾기.java

--- 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
+++ 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -2,7 +2,7 @@ class Solution {
   public String solution(String[] seoul) {
     String answer = "";
     int i = 0;
-    while(i < seoul.length) {
+    while(i < seoul.length) { // for(int i = 0;i < seoul.length;i++)
       if(seoul[i].equals("Kim")) {
         answer = "김서방은 " + i + "에 있다";
       }
