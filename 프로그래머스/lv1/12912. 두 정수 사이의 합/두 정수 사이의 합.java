62333fc14a10bb77d571f9f3783eeb5a4d6a5d3b
yelimkin <77622069+yelimkin@users.noreply.github.com>
2023-06-14 21:12:49 +0900

    Update 두 정수 사이의 합.java

--- 프로그래머스/lv1/12912. 두 정수 사이의 합/두 정수 사이의 합.java
+++ 프로그래머스/lv1/12912. 두 정수 사이의 합/두 정수 사이의 합.java
@@ -1,8 +1,8 @@
 class Solution {
-  public long solution(int a, int b) {
-    long answer = a;
-    int num = b - a;
-    while(num != 0) {
+  public long solution(int a, int b) { // a = -1, b = 3
+    long answer = a; // -1
+    int num = b - a; // 4
+    while(num != 0) { 
       if(num > 0) {
         answer += (++a);
         num--;
@@ -11,6 +11,11 @@ class Solution {
         num++;
       }
     }
+    // num = 4, a = -1 -> answer = -1 + 0 = -1
+    // num = 3, a = 0 -> answer = -1 + 1 = 0
+    // num = 2, a = 1 -> answer = 0 + 2 = 2
+    // num = 1, a = 2 -> answer = 2 + 3 = 5
+    // num = 0
         
     return answer;
   }
