108da969bdb0d0cd45627f89850f5c80fed8a18f
yelimkin <77622069+yelimkin@users.noreply.github.com>
2023-07-22 01:08:06 +0900

    [level 1] Title: 실패율, Time: 6.18 ms, Memory: 73.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42889. 실패율/실패율.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int N, int[] stages) {
+        double clear = 0;
+        int tot_clear = 0;
+
+        HashMap<Integer, Double> map = new HashMap<>();
+
+        for (int i = 1; i < (N + 1); i++) {
+            for (int j = 0; j < stages.length; j++) {
+                if (i == stages[j]) {
+                    clear++;
+                }
+                if (i <= stages[j]) {
+                    tot_clear++;
+                }
+            }
+            map.put(i, clear / ((tot_clear == 0) ? 1 : tot_clear));
+
+            clear = 0;
+            tot_clear = 0;
+        }
+
+        List<Integer> keySet = new ArrayList<>(map.keySet());
+        keySet.sort((o1, o2) -> map.get(o2).compareTo(map.get(o1)));
+
+        return keySet.stream().mapToInt(i -> i).toArray();
+    }
+}
