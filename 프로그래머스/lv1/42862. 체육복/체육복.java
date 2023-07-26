8f2f3a4ca440a997a68e526d459be5daa843af19
yelimkin <77622069+yelimkin@users.noreply.github.com>
2023-07-26 17:18:40 +0900

    [level 1] Title: 체육복, Time: 2.01 ms, Memory: 73.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42862. 체육복/체육복.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+class Solution {
+    public int solution(int n, int[] lost, int[] reserve) {
+        int lost_cnt  = lost.length;
+        Arrays.sort(lost);
+        Arrays.sort(reserve);
+        List<Integer> lost_list = Arrays.stream(lost).boxed().collect(Collectors.toList());
+        List<Integer> rsrv_list = Arrays.stream(reserve).boxed().collect(Collectors.toList());
+
+        for(int lst : lost) {
+            if(rsrv_list.contains(lst)) {
+                lost_list.remove(lost_list.indexOf(lst));
+                rsrv_list.remove(rsrv_list.indexOf(lst));
+                lost_cnt--;
+            }
+        }
+
+        Integer[] ll = lost_list.toArray(new Integer[0]);
+        Integer[] rl = rsrv_list.toArray(new Integer[0]);
+
+        for(int i=0;i < ll.length;i++) {
+            for(int j=0;j < rl.length;j++) {
+                if((ll[i] - 1) == rl[j] || (ll[i] + 1) == rl[j]) {
+                    ll[i] = -1;
+                    rl[j] = -1;
+                    lost_cnt--;
+                }
+            }
+        }
+
+        return n - lost_cnt;
+    }
+}
