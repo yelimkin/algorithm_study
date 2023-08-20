65408e15001480d312027ac623237fb9839c4ca3
yelimkin <77622069+yelimkin@users.noreply.github.com>
2023-08-20 23:35:51 +0900

    [Bronze V] Title: 아스키 코드, Time: 136 ms, Memory: 14260 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String alpha = br.readLine();
+
+        System.out.println((int)alpha.charAt(0));
+    }
+}
