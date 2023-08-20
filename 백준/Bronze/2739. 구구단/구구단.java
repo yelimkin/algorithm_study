70d7f6bb00e20f9a6791ad97611166a679903133
yelimkin <77622069+yelimkin@users.noreply.github.com>
2023-08-20 23:27:20 +0900

    [Bronze V] Title: 구구단, Time: 164 ms, Memory: 16164 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        for(int i=1;i < 10;i++) {
+            System.out.println(n + " * " + i + " = " + (n*i));
+        }
+    }
+}
