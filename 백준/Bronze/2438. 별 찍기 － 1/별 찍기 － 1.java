93f10a1c821684aef6bcf9cde45b2306d074f3d0
yelimkin <77622069+yelimkin@users.noreply.github.com>
2023-08-20 23:24:05 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 124 ms, Memory: 14152 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        for(int i=1;i <= n;i++) {
+            System.out.println("*".repeat(i));
+        }
+
+
+    }
+
+}
