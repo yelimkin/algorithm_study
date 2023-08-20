7acfaa64a279b44117383d6395c260b1bfc2151d
yelimkin <77622069+yelimkin@users.noreply.github.com>
2023-08-20 23:30:39 +0900

    [Bronze V] Title: 시험 성적, Time: 128 ms, Memory: 14124 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -0,0 +1,23 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int score = Integer.parseInt(br.readLine());
+
+        if(score > 89) {
+            System.out.println("A");
+        }else if(score > 79) {
+            System.out.println("B");
+        }else if(score > 69) {
+            System.out.println("C");
+        }else if(score > 59) {
+            System.out.println("D");
+        }else {
+            System.out.println("F");
+        }
+
+    }
+}
