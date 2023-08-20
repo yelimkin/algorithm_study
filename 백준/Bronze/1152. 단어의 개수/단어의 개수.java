005cda08dc373e6ba2cff5d927bb6b96df7a0798
yelimkin <77622069+yelimkin@users.noreply.github.com>
2023-08-20 23:21:25 +0900

    [Bronze II] Title: 단어의 개수, Time: 272 ms, Memory: 20056 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        System.out.println(st.countTokens());
+    }
+}
