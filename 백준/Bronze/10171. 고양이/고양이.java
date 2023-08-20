714094cf8857871f8735924f06d2a8b4beb02ca7
yelimkin <77622069+yelimkin@users.noreply.github.com>
2023-08-20 23:31:06 +0900

    [Bronze V] Title: 고양이, Time: 116 ms, Memory: 13876 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,9 @@
+class Main {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+
+    }
+}
