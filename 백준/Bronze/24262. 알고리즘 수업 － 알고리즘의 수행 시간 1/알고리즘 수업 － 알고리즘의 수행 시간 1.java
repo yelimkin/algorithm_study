d1fc2d75a49a833c6eb839ed232558ab33ec3fe5
yelimkin <77622069+yelimkin@users.noreply.github.com>
2023-10-26 16:26:40 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 124 ms, Memory: 14104 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        // 문제의 알고리즘과 같이 
+        // (/, 내림)연산을 하고 배열에서 인덱스를 통해 값을 찾아 반환(O(1)) 경우,
+        // O(1)
+        int n = Integer.parseInt(br.readLine());
+        
+        System.out.println(1);
+        System.out.println(0);
+    }
+}
