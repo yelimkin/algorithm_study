15bf11143c2c6cf4e6c843460d8cdba35ba70a75
yelimkin <77622069+yelimkin@users.noreply.github.com>
2023-10-26 16:45:52 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 120 ms, Memory: 14220 KB -BaekjoonHub

--- 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.java
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.java
@@ -9,6 +9,7 @@ public class Main {
         // 문제의 알고리즘과 같이 
         // (/, 내림)연산을 하고 배열에서 인덱스를 통해 값을 찾아 반환(O(1)) 경우,
         // O(1)
+        // -> 데이터가 증가하여도 n이 증가해도 연산 횟수는 변하지 않기 때문이다.
         int n = Integer.parseInt(br.readLine());
         
         System.out.println(1);
