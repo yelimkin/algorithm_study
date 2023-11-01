3ceb6508e87f2eaebfe5a7063b5fab36ee7343b8
yelimkin <77622069+yelimkin@users.noreply.github.com>
2023-11-01 15:23:24 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 1008 ms, Memory: 103164 KB -BaekjoonHub

---
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.java
@@ -0,0 +1,72 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Main {
+    static int N;
+    static int M;
+    static ArrayList<Integer>[] lst;
+    static int seq;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] nmr = br.readLine().split(" ");
+
+        // 정점의 수
+        N = Integer.parseInt(nmr[0]);
+        // 간선의 수
+        M = Integer.parseInt(nmr[1]);
+        // 시작 정점
+        int R = Integer.parseInt(nmr[2]);
+
+        // 인접 리스트
+        lst = new ArrayList[N+1];
+
+        // 각 정점에 연결된 정점들을 저장할 ArrayList 초기화
+        for(int l=1;l < N+1;l++) {
+            lst[l] = new ArrayList<>();
+        }
+
+        for(int i=0;i < M;i++) {
+            String[] link = br.readLine().split(" ");
+
+            int a = Integer.parseInt(link[0]);
+            int b = Integer.parseInt(link[1]);
+
+            lst[a].add(b);
+            lst[b].add(a);
+        }
+
+        for(ArrayList node : lst) {
+            if(node != null) {
+                Collections.sort(node);
+            }
+        }
+
+        boolean[] visited = new boolean[N+1];
+        int[] answer = new int[N+1];
+
+        seq = 1;
+        dfs(R, visited, answer);
+
+        StringBuilder sb = new StringBuilder();
+        for(int k=1;k < answer.length;k++) {
+            sb.append(answer[k]);
+            sb.append("\n");
+        }
+
+        System.out.println(sb);
+    }
+
+    static void dfs(int start, boolean[] visited, int[] answer) {
+        visited[start] = true;
+        answer[start] = seq++;
+
+        for(int j=0;j < lst[start].size();j++) {
+            if(!visited[lst[start].get(j)]) {
+                dfs(lst[start].get(j), visited, answer);
+            }
+        }
+    }
+}
