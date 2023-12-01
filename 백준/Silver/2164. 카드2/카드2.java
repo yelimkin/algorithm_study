754771c3acf9499d5bcfa57e438d2f79468e3894
yelimkin <77622069+yelimkin@users.noreply.github.com>
2023-12-01 21:23:27 +0900

    [Silver IV] Title: 카드2, Time: 196 ms, Memory: 53508 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -1,27 +1,32 @@
 import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
 import java.util.Queue;
+import java.util.LinkedList;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
 
+        int N = Integer.parseInt(br.readLine());
+        // 차례로 꺼내면서 확인할 큐
         Queue<Integer> q = new LinkedList<>();
-        for(int i=0;i < n;i++) {
-            q.offer(i+1);
+        for(int i=1;i <= N;i++) {
+            q.offer(i);
         }
 
+        // true : 맨 위 카드 버리기, false : 맨 위 카드를 맨 뒤로 보내기
+        boolean check = true;
         while(q.size() > 1) {
-            q.remove();
-            if(q.size() == 1) break;
-            int tmp = q.peek();
-            q.remove();
-            q.offer(tmp);
+            if(check) {
+                q.poll();
+            }else {
+                int back = q.poll();
+                q.offer(back);
+            }
+            check = !check;
         }
-
-        System.out.println(q.peek());
+        System.out.println(q.poll());
+        br.close();
     }
 }
