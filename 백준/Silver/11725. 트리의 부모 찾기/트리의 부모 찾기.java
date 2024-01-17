f8103f5bec30aa64d2b5c5aa1e27fd474e694c3d
yelimkin <77622069+yelimkin@users.noreply.github.com>
2024-01-17 18:46:09 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 876 ms, Memory: 86364 KB -BaekjoonHub

---
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.HashMap;
+import java.util.Queue;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+
+        // 트리의 루트 노드는 1번 노드
+        HashMap<Integer, ArrayList<Integer>> tree = new HashMap<>();
+        for(int i=1;i <= N;i++) {
+            tree.put(i, new ArrayList<>());
+        }
+
+        boolean[] check = new boolean[N+1];
+        int[] answer = new int[N+1];
+
+        for(int i=1;i < N;i++) {
+            String[] input = br.readLine().split(" ");
+
+            int a = Integer.parseInt(input[0]);
+            int b = Integer.parseInt(input[1]);
+
+            tree.get(a).add(b);
+            tree.get(b).add(a);
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+        q.add(1);
+        while(!q.isEmpty()) {
+            int tmp = q.poll();
+            ArrayList<Integer> node = tree.get(tmp);
+
+            if(!check[tmp]) {
+                for(Integer num : node) {
+                    if(!check[num]) {
+                        answer[num] = tmp;
+                        q.add(num);
+                    }
+                }
+                check[tmp] = true;
+            }
+        }
+
+        for(int k=2;k < answer.length;k++) {
+            sb.append(answer[k]).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
